d1cd355b0cca50531a1c3cd1e2edb92eaad35856
SAPNA080402 <sapnasivagnanam@gmail.com>
2023-11-21 08:18:24 +0530

    added java task

---
+++ AsciiTask7.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class AsciiTask7{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int charInput = (int) sc.next().charAt(0);
+		System.out.println(charInput);
+	}
+}
